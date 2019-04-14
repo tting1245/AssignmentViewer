1fee5e5fb6c27ad50137f2a7ac8c402bed36de55
tting1245 <48236404+tting1245@users.noreply.github.com>
2019-04-14 22:42:36 +0900

    Add files via upload

--- Community Site/public/js/style.js
+++ Community Site/public/js/style.js
@@ -1,3 +1,17 @@
 $(function() {
     $(document),tooltip();  //body에 있는 모든 태그 중에 title이란 속성값이 존재하는 태그들에 마우스를 오버하면 툴팁기능 동작
 })
+
+jQuery(document).ready(function() {
+    $('.scroll1').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+jQuery(document).ready(function() {
+    $('.scroll2').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
